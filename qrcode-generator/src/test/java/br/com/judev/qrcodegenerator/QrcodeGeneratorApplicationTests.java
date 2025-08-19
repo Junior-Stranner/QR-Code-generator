320daefd332f69194f4ed877c02b1003e3d948eb
Junior-Stranner <JuniorStranner@gmail.com>
2025-08-19 09:52:12 -0300

    add some new dependencies like google zxing and aws s3

---
+++ qrcode-generator/src/test/java/br/com/judev/qrcodegenerator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
